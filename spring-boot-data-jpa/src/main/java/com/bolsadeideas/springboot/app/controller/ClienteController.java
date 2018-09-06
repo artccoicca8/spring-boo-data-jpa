7911b65054fb95c33128b11f92eff7cf411a6f4a
artccoicca8 <artccoicca8@gmail.com>
2018-09-06 01:47:06 -0500

    video 60

--- spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/controller/ClienteController.java
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/controller/ClienteController.java
@@ -15,6 +15,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -29,99 +30,113 @@ import com.bolsadeideas.springboot.app.models.service.IClienteService;
 import com.bolsadeideas.springboot.app.util.paginator.PageRender;
 
 @Controller
-@SessionAttributes("cliente") // permite setear el objeto cliente en la session 
+@SessionAttributes("cliente") // permite setear el objeto cliente en la session
 public class ClienteController {
 
 	@Autowired
-	private IClienteService clienteService; 
-	
-	@RequestMapping(value="/listar",method=RequestMethod.GET)
-	public String listar ( @RequestParam(name="page", defaultValue="0") int page, Model model ) {
-		
-		// este el el pagina , indica el numero y la cantidad de columnas de 5 en 5 
-		Pageable pageable = new PageRequest(page, 5); 
-		
-		Page<Cliente> clientes = clienteService.findAll(pageable);
-		PageRender< Cliente> pageRender = new PageRender<>("/listar", clientes );
-		model.addAttribute("titulo","listado de clientes ");
-		model.addAttribute("clientes", clientes); 
-		model.addAttribute("page", pageRender); 
-		return "listar";
-		
-	
+	private IClienteService clienteService;
+
+	@GetMapping(value = "/ver/{id}")
+	public String ver(@PathVariable(value = "id") Long id, Map<String, Object> model ,RedirectAttributes flash) {
+
+		Cliente cliente = clienteService.findOne(id);
+
+		if (cliente == null) {
+			flash.addFlashAttribute("error", "ID Cliente no existe en bd ");
+			return "redirect:listar";
+
+		} 
+		model.put("titulo", "Detalle Cliente");
+		model.put("cliente", cliente);
+		return "ver";
 	}
-	
-	@RequestMapping(value="/form")
-	public String create(Map<String,Object> model) {
-		
-		Cliente cliente = new Cliente(); 
+
+	@RequestMapping(value = "/listar", method = RequestMethod.GET)
+	public String listar(@RequestParam(name = "page", defaultValue = "0") int page, Model model) {
+
+		// este el el pagina , indica el numero y la cantidad de columnas de 5 en 5
+		Pageable pageable = new PageRequest(page, 5);
+
+		Page<Cliente> clientes = clienteService.findAll(pageable);
+		PageRender<Cliente> pageRender = new PageRender<>("/listar", clientes);
+		model.addAttribute("titulo", "listado de clientes ");
+		model.addAttribute("clientes", clientes);
+		model.addAttribute("page", pageRender);
+		return "listar";
+
+	}
+
+	@RequestMapping(value = "/form")
+	public String create(Map<String, Object> model) {
+
+		Cliente cliente = new Cliente();
 		model.put("titulo", "Formulario Cliente");
 		model.put("cliente", cliente);
-		return "form"; 
+		return "form";
 	}
-	
-	@RequestMapping(value="/form",method=RequestMethod.POST)
-	public String wardar(@Valid Cliente cliente ,BindingResult result, Model model,@RequestParam("file")  MultipartFile foto   ,RedirectAttributes flash, SessionStatus status) {
-		
+
+	@RequestMapping(value = "/form", method = RequestMethod.POST)
+	public String wardar(@Valid Cliente cliente, BindingResult result, Model model,
+			@RequestParam("file") MultipartFile foto, RedirectAttributes flash, SessionStatus status) {
+
 		if (result.hasErrors()) {
-			model.addAttribute("titulo", "Formulario Cliente"); 
+			model.addAttribute("titulo", "Formulario Cliente");
 			return "form";
 		}
-		
-		
-		if (foto.isEmpty()) {
+
+		if (!foto.isEmpty()) {
 			Path directorioRecursos = Paths.get("src//main//resources//static//uploads");
-			String rootPath = directorioRecursos.toFile().getAbsolutePath(); 
+			String rootPath = directorioRecursos.toFile().getAbsolutePath();
 			try {
 				byte[] bytes = foto.getBytes();
-				Path rutaCompleta = Paths.get(rootPath+"//"+foto.getOriginalFilename());
-				Files.write(rutaCompleta, bytes); 
-				flash.addFlashAttribute ("info","Has subido corectamente "+foto.getOriginalFilename());
+				Path rutaCompleta = Paths.get(rootPath + "//" + foto.getOriginalFilename());
+				Files.write(rutaCompleta, bytes);
+				flash.addFlashAttribute("info", "Has subido corectamente '" + foto.getOriginalFilename() + "'");
 				cliente.setFoto(foto.getOriginalFilename());
-				
 			} catch (IOException e) {
 				e.printStackTrace();
-			} 
+
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
 		}
-		
-		
-		String msgFlash = (cliente.getId()==null )? "Cliente Creado con éxito ":" Cliente Editado con Exito ";
-		
+
+		String msgFlash = (cliente.getId() == null) ? "Cliente Creado con éxito " : " Cliente Editado con Exito ";
+
 		clienteService.save(cliente);
 		status.setComplete();
 		flash.addFlashAttribute("success", msgFlash);
-		return "redirect:/listar"; 
+		// flash.addFlashAttribute("warning", msgFlash);
+		return "redirect:/listar";
 	}
-	
-	
-	@RequestMapping(value="/form/{id}")
-	public String edit(@PathVariable(value="id") Long id,RedirectAttributes flash,  Map<String,Object> model ) {
-		
+
+	@RequestMapping(value = "/form/{id}")
+	public String edit(@PathVariable(value = "id") Long id, RedirectAttributes flash, Map<String, Object> model) {
+
 		Cliente cliente = null;
-		if (id>0) {
-			cliente=clienteService.findOne(id);
-			if (cliente==null) {
+		if (id > 0) {
+			cliente = clienteService.findOne(id);
+			if (cliente == null) {
 				flash.addFlashAttribute("error", "ID Cliente no existe en bd ");
-				return "redirect:listar"; 
+				return "redirect:listar";
 			}
-		}else {
+		} else {
 			flash.addFlashAttribute("error", "ID Cliente no 0 ");
-			return "redirect:listar"; 
+			return "redirect:listar";
 		}
 		model.put("titulo", "Formulario Cliente");
 		model.put("cliente", cliente);
-		return "form"; 
+		return "form";
 	}
-	
-	
-	@RequestMapping(value="/eliminar/{id}")
-	public String delete(@PathVariable(value="id") Long id,RedirectAttributes flash) {
-		
-		if (id>0) {
+
+	@RequestMapping(value = "/eliminar/{id}")
+	public String delete(@PathVariable(value = "id") Long id, RedirectAttributes flash) {
+
+		if (id > 0) {
 			clienteService.delete(id);
 			flash.addFlashAttribute("success", "Cliente eliminado con exito ");
 		}
-		return "redirect:/listar"; 
+		return "redirect:/listar";
 	}
-	
+
 }
